16119370b3e1e3b9eb6a6838c95da37016986a5e
Maliksalmon2008 <83311416+Maliksalmon2008@users.noreply.github.com>
2021-07-31 20:44:54 -0700

    Add files via upload

---
+++ main.js
@@ -0,0 +1,72 @@
+var mouseEvent =[]
+var canvas =[]
+var lastpositionX;
+var lastpositionY;
+var color="blue";
+var width="10";
+var touchEvent=[];
+canvas = document.getElementById("myCanvas");
+ctx = canvas.getContext("2d");
+
+
+
+canvas.addEventListener("mousedown",my_mousedown);
+function my_mousedown(e){
+color=document.getElementById("color").value;
+width=document.getElementById("width").value;
+mouseEvent="mouseDown";
+}
+canvas.addEventListener("mouseup",my_mouseup);
+function my_mouseup(e){
+mouseEvent="mouseUp";
+}
+
+canvas.addEventListener("mouseleave",my_mouseleave);
+function my_mouseleave(e){
+mouseEvent="mouseleave";
+}
+
+canvas.addEventListener("mousemove",my_mousemove);
+function my_mousemove(e){
+
+ currentpositionofX=e.clientX - canvas.offsetLeft;
+ currentpositionofY=e.clientY - canvas.offsetTop;
+if(mouseEvent=="mouseDown"){
+ ctx.beginPath();
+ ctx.strokeStyle=color;
+ ctx.lineWidth=width;
+ ctx.moveTo(lastpositionX,lastpositionY);
+
+ console.log("last position of X and Y cooradnites")
+ console.log("X="+lastpositionX+"Y"+lastpositionY);
+ ctx.lineTo(currentpositionofX,currentpositionofY);
+ ctx.stroke();
+ console.log("Current position of X and Y cooradnites")
+ console.log("X="+currentpositionX+"Y"+currentpositionY);
+}
+lastpositionX=currentpositionofX;
+ lastpositionY=currentpositionofY;
+
+}
+function touch_move(){
+    currentpositionofX=e.clientX - canvas.offsetLeft;
+    currentpositionofY=e.clientY - canvas.offsetTop;
+   if(touchEvent=="touch_move"){
+    ctx.beginPath();
+    ctx.strokeStyle=color;
+    ctx.lineWidth=width;
+    ctx.moveTo(lastpositionX,lastpositionY);
+   
+    console.log("last position of X and Y cooradnites")
+    console.log("X="+lastpositionX+"Y"+lastpositionY);
+    ctx.lineTo(currentpositionofX,currentpositionofY);
+    ctx.stroke();
+    console.log("Current position of X and Y cooradnites")
+    console.log("X="+currentpositionX+"Y"+currentpositionY);
+   }
+   lastpositionX=currentpositionofX;
+    lastpositionY=currentpositionofY;    
+}
+function clear_area(){
+    ctx.clearRect(0,0, ctx.canvas.width,ctx.canvas.height);
+}
